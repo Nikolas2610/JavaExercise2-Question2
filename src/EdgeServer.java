764c1886e5a4fb1e7c279dad949847d76b5913d9
Nikolas2610 <npsillou@gmail.com>
2023-02-06 19:46:54 +0200

    New data structrure for the graph

--- src/EdgeServer.java
+++ src/EdgeServer.java
@@ -9,6 +9,11 @@ public class EdgeServer extends Thread {
 
     private final Socket clientSocket;
     private final int clientId;
+    private static Graph graph;
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_RESET = "\u001B[0m";
 
     private EdgeServer(Socket client, int id) {
         clientSocket = client;
@@ -17,6 +22,8 @@ public class EdgeServer extends Thread {
     }
 
     public static void main(String[] args) throws IOException {
+        // Get the random Graph
+        graph = new Graph(3000,30000);
 //      Initialize socket server with port
         int count = 0;
         ServerSocket serverSocket = null;
@@ -25,9 +32,10 @@ public class EdgeServer extends Thread {
             serverSocket = new ServerSocket(PORT);
             System.out.println("Server listening at port " + PORT);
             try {
+//                Create multiple connections with clients
                 while (true) {
                     new EdgeServer(serverSocket.accept(), count);
-                    System.out.println("\033[34mClient " + count + " connected to server and waiting for connection...\033[0m");
+                    System.out.println(ANSI_BLUE + "Client " + count + " connected to server and waiting for connection..." + ANSI_RESET);
                     count++;
                 }
             } catch (IOException e) {
@@ -46,38 +54,20 @@ public class EdgeServer extends Thread {
                 System.exit(1);
             }
         }
-////      Server connect and waiting for client
-//        Socket clientSocket = null;
-//        System.out.println("Server listening at port " + PORT);
-//        System.out.println("Waiting for connection.....");
-////      Server accepting the client
-//        try {
-//            clientSocket = serverSocket.accept();
-//        } catch (IOException e) {
-////            Catch the errors
-//            System.err.println("Accept failed.");
-//            System.exit(1);
-//        }
-////      Print success message from the connection
-//        System.out.println("Connection successful");
-//        System.out.println("Waiting for input.....");
-//      Initialize the In/0ut response from the client
-
     }
+
 
     public void run() {
         try {
+//          Set variables to read and write to the client
             PrintWriter out = new PrintWriter(this.clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String inputLine;
 
-//      Get the random Graph
-            EdgeList edgeList = new EdgeList();
-
             while (true) {
 //          Server send message to client for the details tha wants
-                out.println("Write to keyboard the edge. Nodes are " + edgeList.getNODES() +
-                        " and the accept format is X,X with max value " + (edgeList.getNODES() - 1)
+                out.println("Write to keyboard the edge. Nodes are " + graph.getNODES() +
+                        " and the accept format is X,X with max value " + (graph.getNODES() - 1)
                         + " (example: 4,2)\nFor exit write -1,-1:");
 //          Server waiting for response from client
                 inputLine = in.readLine();
@@ -98,19 +88,19 @@ public class EdgeServer extends Thread {
                         }
 //                  Check if the user want to close the connection
                         if (edges[0].equals("-1") && edges[1].equals("-1")) {
-                            System.out.println("\033[31mClient " + this.clientId +  " has been disconnected from server\033[0m");
+                            System.out.println(ANSI_RED + "Client " + this.clientId +  " has been disconnected from server" + ANSI_RESET);
                             out.println("Close connection with server!");
                             break;
                         }
 //                  Check if the user gives available numbers with the min and max nodes
-                        if (edgesInt[0] >= edgeList.getNODES() || edgesInt[1] >= edgeList.getNODES() ||
+                        if (edgesInt[0] >= graph.getNODES() || edgesInt[1] >= graph.getNODES() ||
                                 edgesInt[0] < 0 || edgesInt[1] < 0) {
                             out.println("Wrong input");
                             continue;
                         }
 //                  Check if the edge exists to the graph and create message for the client
-                        String message = edgeList.edgeExists(edgesInt[0], edgesInt[1]);
-                        System.out.println("\033[33mClient " + this.clientId +  ": Row: " + edges[0] + " Col: " + edges[1] + "\033[0m");
+                        String message = graph.edgeExists(edgesInt[0], edgesInt[1]);
+                        System.out.println(ANSI_YELLOW + "Client " + this.clientId +  ": Row: " + edges[0] + " Col: " + edges[1] + ANSI_RESET);
 //                  Send the server response to the client
                         out.println(message);
                     } catch (NumberFormatException e) {
